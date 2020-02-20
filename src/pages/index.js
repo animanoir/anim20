c97e0bc29ddbadea81bb0f86b329525cfafa2c5d
Óscar A. Montiel <etherealflux94@gmail.com>
2020-02-20 01:23:55 -0600

    Three.js now works.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
-import React, { Children, useRef, useState } from "react"
-import {  Link } from "gatsby"
+import React from "react"
+// import {  Link } from "gatsby"
 import Layout from "../components/layout"
+import { Canvas } from "react-three-fiber"
 
 const IndexPage = () => {
   return(
     <Layout>
       <div>
         <h1>Welcome to Animanoir.</h1>
+      </div>
+      <div>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry
+            attach="geometry"
+            arcs={[6, 6, 6]}
+          />
+          <meshBasicMaterial attach="material" color="red" />
+        </mesh>
+      </Canvas>
+
       </div>
     </Layout>
     )
